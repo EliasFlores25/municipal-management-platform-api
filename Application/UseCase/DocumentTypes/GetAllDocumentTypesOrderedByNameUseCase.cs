ac22560d0f414919f5ffa7fa31b96099d28b82bb
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-06-22 17:01:05 -0600

    feat(application): implementread queries for DocumentType

---
+++ Application/UseCase/DocumentTypes/GetAllDocumentTypesOrderedByNameUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Abstractions;
+using static Application.DTOs.DocumentTypeDtos;
+
+namespace Application.UseCases.DocumentTypes
+{
+    public class GetAllDocumentTypesOrderedByNameUseCase
+    {
+        private readonly IDocumentTypeRepository _documentTypeRepository;
+
+        public GetAllDocumentTypesOrderedByNameUseCase(IDocumentTypeRepository documentTypeRepository)
+            => _documentTypeRepository = documentTypeRepository;
+        public async Task<IEnumerable<DocumentTypeResponse>> ExecuteAsync()
+
+            => (await _documentTypeRepository.GetAllOrderedByNameAsync())
+                                             .Select(dt => new DocumentTypeResponse(dt.Id, dt.Name));
+    }
+}
---
+++ Application/UseCase/DocumentTypes/GetDocumentTypeByIdUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Application.Exceptions;
+using Domain.Abstractions;
+using static Application.DTOs.DocumentTypeDtos;
+
+namespace Application.UseCases.DocumentTypes
+{
+    public class GetDocumentTypeByIdUseCase
+    {
+        private readonly IDocumentTypeRepository _documentTypeRepository;
+
+        public GetDocumentTypeByIdUseCase(IDocumentTypeRepository documentTypeRepository)
+            => _documentTypeRepository = documentTypeRepository;
+
+        public async Task<DocumentTypeResponse> ExecuteAsync(int id)
+        {
+            var documentType = await _documentTypeRepository.GetByIdAsync(id);
+            if (documentType is null)
+            {
+                throw new ApplicationValidationException($"No se encontró el tipo de documento con ID {id}.");
+            }
+
+            return new DocumentTypeResponse(documentType.Id, documentType.Name);
+        }
+    }
+}
