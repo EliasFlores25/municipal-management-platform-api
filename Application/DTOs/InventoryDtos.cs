1f1d4b58a4a473a7672d47734f7751dbc608ba3a
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-06-24 12:54:45 -0600

    feat(application): add Inventory DTOs and Mapster mapping configuration

---
+++ Application/DTOs/InventoryDtos.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Application.DTOs
+{
+    public static class InventoryDtos
+    {
+        public record InventoryCreateRequest(
+                     string ItemName,
+                     string Description,
+                     int Quantity,
+                     int MunicipalityId,
+                     string ImageUrl);
+
+        public record InventoryUpdateInfoRequest(
+                    int Id,
+                    string Description,
+                    string ImageUrl);
+
+        public record InventoryStockAdjustmentRequest(
+                    int Id,
+                    int Amount);
+
+        public record InventoryResponse(
+                    int Id,
+                    string ItemName,
+                    string Description,
+                    int Quantity,
+                    DateTime EntryDate,
+                    string State,
+                    string ImageUrl,
+                    int MunicipalityId,
+                    string MunicipalityName);
+    }
+}
---
+++ Application/Mappings/InventoryMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Mapster;
+using static Application.DTOs.InventoryDtos;
+
+namespace Application.Mappings
+{
+    public class InventoryMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Inventory, InventoryResponse>()
+                .Map(dest => dest.State, src => src.State.ToString())
+                .Map(dest => dest.MunicipalityName, src => src.Municipality != null ? src.Municipality.Name : "No Asignado");
+        }
+    }
+}
