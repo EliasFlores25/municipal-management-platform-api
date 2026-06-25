8e7a1a2b687f5627b59bbec352f3b2e65d515fe5
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-06-25 06:21:26 -0600

    feat(application): add Project DTOs and Mapster mapping configuration

---
+++ Application/DTOs/ProjectDtos.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Application.DTOs
+{
+    public static class ProjectDtos
+    {
+        public record ProjectCreateRequest(
+            string Name,
+            string Description,
+            DateTime EndDate,
+            decimal Budget,
+            int MunicipalityId);
+
+        public record ProjectUpdatePlanningRequest(
+            int Id,
+            string Name,
+            string Description,
+            DateTime EndDate,
+            decimal Budget);
+
+        public record ProjectResponse(
+            int Id,
+            string Name,
+            string Description,
+            DateTime StartDate,
+            DateTime EndDate,
+            decimal Budget,
+            string State,
+            int MunicipalityId,
+            string MunicipalityName);
+    }
+}
---
+++ Application/Mappings/ProjectMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Mapster;
+using static Application.DTOs.ProjectDtos;
+
+namespace Application.Mappings
+{
+    public class ProjectMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Project, ProjectResponse>()
+                .Map(dest => dest.State, src => src.State.ToString())
+                .Map(dest => dest.MunicipalityName, src => src.Municipality != null ? src.Municipality.Name : "No Asignado");
+        }
+    }
+}
