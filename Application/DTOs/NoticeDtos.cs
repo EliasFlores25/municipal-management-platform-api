217e28886385f9bb20e9ed3a7f119d28e3460b13
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-06-27 17:19:59 -0600

    feat(application): add Notice DTOs and Mapster mapping configuration

---
+++ Application/DTOs/NoticeDtos.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs
+{
+    public static class NoticeDtos
+    {
+        public record NoticeCreateRequest(
+            string Title,
+            string Description,
+            NoticeCategory Category,
+            int MunicipalityId);
+
+        public record NoticeUpdateContentRequest(
+            int Id,
+            string Title,
+            string Description,
+            NoticeCategory Category);
+
+        public record NoticeResponse(
+            int Id,
+            string Title,
+            string Description,
+            DateTime RegistrationDate,
+            string Category,
+            bool IsArchived,
+            int MunicipalityId,
+            string MunicipalityName);
+    }
+}
---
+++ Application/Mappings/NoticeMappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Mapster;
+using static Application.DTOs.NoticeDtos;
+
+namespace Application.Mappings;
+
+public class NoticeMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Notice, NoticeResponse>()
+            .Map(dest => dest.Category, src => src.Category.ToString())
+            .Map(dest => dest.MunicipalityName, src => src.Municipality != null ? src.Municipality.Name : "No Asignado");
+    }
+}
