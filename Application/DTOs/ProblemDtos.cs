6bb6ee9769585481d78652155288f2bf95b22a6e
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-06-29 23:06:55 -0600

    feat(application): add Problem DTOs and Mapster mapping configuration

---
+++ Application/DTOs/ProblemDtos.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs
+{
+    public static class ProblemDtos
+    {
+
+        public record ProblemCreateRequest(
+            string Title,
+            string Description,
+            ProblemType Type,
+            ProblemSeverity Severity,
+            int MunicipalityId);
+
+        public record ProblemUpdateReportRequest(
+            int Id,
+            string Title,
+            string Description,
+            ProblemType Type,
+            ProblemSeverity Severity);
+
+        public record ProblemResponse(
+            int Id,
+            string Title,
+            string Description,
+            DateTime RegistrationDate,
+            string Type,
+            string Severity,
+            string Status,
+            int MunicipalityId,
+            string MunicipalityName);
+    }
+}
---
+++ Application/Mappings/ProblemMappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Mapster;
+using static Application.DTOs.ProblemDtos;
+
+namespace Application.Mappings;
+
+public class ProblemMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Problem, ProblemResponse>()
+            .Map(dest => dest.Type, src => src.Type.ToString())
+            .Map(dest => dest.Severity, src => src.Severity.ToString())
+            .Map(dest => dest.Status, src => src.Status.ToString())
+            .Map(dest => dest.MunicipalityName, src => src.Municipality != null ? src.Municipality.Name : "No Asignado");
+    }
+}
