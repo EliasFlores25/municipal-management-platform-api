885daa1261a414e4d2d5f57a55062cb3f8ca0999
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-07-02 13:05:41 -0600

    feat(application): establish Document data transfer objects and Mapster config

---
+++ Application/DTOs/DocumentDtos.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Application.DTOs
+{
+    public static class DocumentDtos
+    {
+        public record DocumentCreateRequest(
+            string DocumentNumber,
+            string Proprietary,
+            string Details,
+            int DocumentTypeId,
+            int MunicipalityId,
+            DateTime? EmissionDate);
+
+        public record DocumentUpdateDetailsRequest(
+            int Id,
+            string Proprietary,
+            string Details);
+
+        public record DocumentResponse(
+            int Id,
+            string DocumentNumber,
+            DateTime EmissionDate,
+            string Proprietary,
+            string Details,
+            string State,
+            int DocumentTypeId,
+            string DocumentTypeName,
+            int MunicipalityId,
+            string MunicipalityName);
+
+    }
+}
---
+++ Application/Mappings/DocumentMappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Mapster;
+using static Application.DTOs.DocumentDtos;
+
+namespace Application.Mappings
+{
+    public class DocumentMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Document, DocumentResponse>()
+                .Map(dest => dest.State, src => src.State.ToString())
+                .Map(dest => dest.DocumentTypeName, src => src.DocumentType != null ? src.DocumentType.Name : "Sin Especificar")
+                .Map(dest => dest.MunicipalityName, src => src.Municipality != null ? src.Municipality.Name : "Sin Municipio");
+        }
+    }
+}
