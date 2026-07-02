96c93a59d050996719f35e2ed56ce0fad1183283
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-07-01 19:04:16 -0600

    feat(application): setup User data transfer objects and Mapster configuration

---
+++ Application/DTOs/UserDtos.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Application.DTOs
+{
+    public static class UserDtos
+    {
+        public record UserCreateRequest(
+            string Name, 
+            string Email, 
+            string Password, 
+            int RoleId);
+
+        public record UserUpdateProfileRequest(
+            int Id, 
+            string Name, 
+            string Email, 
+            int RoleId);
+
+        public record UserChangePasswordRequest(
+            int Id, 
+            string NewPassword);
+
+        public record LoginRequest(
+            string Email, 
+            string Password);
+
+        public record UserResponse(
+            int Id,
+            string Name,
+            string Email,
+            bool IsActive,
+            int RoleId,
+            string RoleName);
+
+        public record LoginResponse(
+            string Token, 
+            UserResponse User);
+
+    }
+}
---
+++ Application/Mappings/UserMappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Mapster;
+using static Application.DTOs.UserDtos;
+
+namespace Application.Mappings
+{
+    public class UserMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<User, UserResponse>()
+                .Map(dest => dest.RoleName, src => src.Role != null ? src.Role.Name : "Sin Rol");
+        }
+    }
+}
