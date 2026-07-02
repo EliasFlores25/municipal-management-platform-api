47904c44f68ebdcd7168e90b50b641604cdf9e81
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-07-02 10:47:49 -0600

    feat(application): integrate Employee data transfer objects and Mapster mapping

---
+++ Application/DTOs/EmployeeDtos.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Application.DTOs
+{
+    public static class EmployeeDtos
+    {
+        public record EmployeeCreateRequest(
+            string Code,
+            string FirstName,
+            string LastName,
+            int PositionId,
+            int MunicipalityId);
+
+        public record EmployeeUpdateProfileRequest(
+            int Id,
+            string FirstName,
+            string LastName,
+            int PositionId,
+            int MunicipalityId);
+
+        public record EmployeeTerminateRequest(int Id, DateTime? ExitDate);
+
+        public record EmployeeReactivateRequest(
+            int Id,
+            int PositionId,
+            int MunicipalityId,
+            DateTime? NewContractDate);
+
+        public record EmployeeResponse(
+            int Id,
+            string Code,
+            string FirstName,
+            string LastName,
+            string FullName,
+            DateTime ContractDate,
+            DateTime? ExitDate,
+            string State,
+            int PositionId,
+            string PositionName,
+            int MunicipalityId,
+            string MunicipalityName,
+            TimeSpan? ContractDuration);
+    }
+}
---
+++ Application/Mappings/EmployeeMappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Mapster;
+using static Application.DTOs.EmployeeDtos;
+
+namespace Application.Mappings
+{
+    public class EmployeeMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Employee, EmployeeResponse>()
+                .Map(dest => dest.State, src => src.State.ToString())
+                .Map(dest => dest.PositionName, src => src.Position != null ? src.Position.Name : "Sin Cargo")
+                .Map(dest => dest.MunicipalityName, src => src.Municipality != null ? src.Municipality.Name : "Sin Municipio");
+        }
+    }
+}
