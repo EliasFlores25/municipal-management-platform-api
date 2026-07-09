85d29b2cdc14420cdee4cc1cbb403f9d561f6bf4
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-07-09 09:43:18 -0600

    feat(infrastructure): register Data layer services and repository dependencies

---
+++ Data/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Data.Persistence;
+using Data.Repositories;
+using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddData(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<IMunicipalityRepository, MunicipalityRepository>();
+            services.AddScoped<IPositionRepository, PositionRepository>();
+            services.AddScoped<IDocumentTypeRepository, DocumentTypeRepository>();
+
+            return services;
+        }
+    }
+}
