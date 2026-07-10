93b6e37f2c6db4539dc7898fe3ae03ab597b89e4
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2026-07-10 15:25:47 -0600

    feat(infrastructure): register Level 2 repository dependencies and Mapster configuration

--- Data/DependencyInjection.cs
+++ Data/DependencyInjection.cs
@@ -1,8 +1,11 @@
 ﻿using Data.Persistence;
 using Data.Repositories;
 using Domain.Abstractions;
+using Mapster;
+using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Data
 {
@@ -12,11 +15,22 @@ namespace Data
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
+            var config = TypeAdapterConfig.GlobalSettings;
+            config.Scan(Assembly.GetExecutingAssembly());
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, Mapper>();
+
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IRoleRepository, RoleRepository>();
             services.AddScoped<IMunicipalityRepository, MunicipalityRepository>();
             services.AddScoped<IPositionRepository, PositionRepository>();
             services.AddScoped<IDocumentTypeRepository, DocumentTypeRepository>();
+
+            services.AddScoped<IInventoryRepository, InventoryRepository>();
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<INoticeRepository, NoticeRepository>();
+            services.AddScoped<IProblemRepository, ProblemRepository>();
+
 
             return services;
         }
